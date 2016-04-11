f8abaa5a7031964d19826a29b74b06e996f93a86
Nate Yonkee <tulanathoar@gmail.com>
2016-04-10 18:55:49 -0600

    make it a sourceable file instead of executable

--- pretty-time.zsh
+++ pretty-time.zsh
@@ -1,19 +1,15 @@
-#!/usr/bin/env zsh
-
-if (( $# == 0 )); then
-	echo 'Input required'
-	exit 1
-fi
-
-local human total_seconds=$1
-local days=$(( total_seconds / 60 / 60 / 24 ))
-local hours=$(( total_seconds / 60 / 60 % 24 ))
-local minutes=$(( total_seconds / 60 % 60 ))
-local seconds=$(( total_seconds % 60 ))
-
+function ppretty-time() {
+local total_seconds="$1"
+days=$(( $total_seconds / 60 / 60 / 24 ))
+hours=$(( $total_seconds / 60 / 60 % 24 ))
+minutes=$(( $total_seconds / 60 % 60 ))
+seconds=$(( $total_seconds % 60 ))
+local human
 (( days > 0 )) && human+="${days}d "
 (( hours > 0 )) && human+="${hours}h "
 (( minutes > 0 )) && human+="${minutes}m "
 human+="${seconds}s"
 
 echo "$human"
+}
+zle -N ppretty-time
